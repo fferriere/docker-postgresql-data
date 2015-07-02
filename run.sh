a09490e04aeee8b92eadf26ec9d771000087fc7f
FERRIERE Florian <ferriere.florian@gmail.com>
2015-07-02 12:35:17 +0200

    Add verification for not re-run a container if data container already runned

--- run.sh
+++ run.sh
@@ -10,6 +10,11 @@ if [ -n "$FFERRIERE_PG_DATA_IMAGE" ]; then
     IMAGE="$FFERRIERE_PG_DATA_IMAGE"
 fi
 
+NB_ROWS=$(docker ps -a | grep -w "$NAME" | grep Exited | wc -l)
+if [ "$NB_ROWS" -gt 0 ]; then
+    exit 0
+fi
+
 docker run -d \
   --name $NAME \
   $IMAGE
