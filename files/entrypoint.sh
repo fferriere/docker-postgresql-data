0e5c189a7ece5cab6ce88a8b5e1f712dc37be423
FERRIERE Florian <ferriere.florian@gmail.com>
2015-10-16 12:30:13 +0200

    Run chmod on run

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 chown -R postgres:postgres /var/lib/postgresql
+chmod -R 700 /var/lib/postgresql
 chown -R postgres:postgres /var/run/postgresql
 chown -R postgres:postgres /var/log/postgresql
 chown -R postgres:postgres /etc/postgresql/
